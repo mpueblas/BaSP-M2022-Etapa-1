8bfe5556a18398b7da4e5cf0beee68829a02b5dd
Martin Pueblas <war.pueblas@gmail.com>
2022-04-16 20:33:45 -0300

    BaSP-W05-2022: ex03-arrays (completed)

--- Semana-05/excercises/03_arrays.js
+++ Semana-05/excercises/03_arrays.js
@@ -9,3 +9,35 @@ console.log(meses [5] +' '+ meses [11]);
 var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
  'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 console.log(meses.sort());
+
+//c.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+meses.push('Final');
+meses.unshift('Principio');
+console.log(meses);
+
+//d.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+meses.pop();
+meses.shift();
+console.log(meses);
+
+//e.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+meses.reverse();
+console.log(meses);
+
+//f.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre,' 'Noviembre', 'Diciembre'];
+var cadena = meses.join();
+console.log(meses);
+
+//g.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+var b = meses.slice(4, 10);
+console.log(b);

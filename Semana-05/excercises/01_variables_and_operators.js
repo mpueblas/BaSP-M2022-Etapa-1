afe6970a3a468b3feb8651ae297d56d625a1ec46
Martin Pueblas <war.pueblas@gmail.com>
2022-04-16 20:17:15 -0300

    BaSP-W05-2022: ex01-variables and operators

--- Semana-05/excercises/01_variables_and_operators.js
+++ Semana-05/excercises/01_variables_and_operators.js
@@ -1,1 +1,28 @@
 console.log('TestingJS script')
+
+//Variables y Operadores
+
+//a.
+var a = 5;
+var b = 6;
+var c = a + b;
+
+console.log(c);
+
+//a.2
+
+var a1 = 'Bienvenidos a ';
+var b1 = 'mi practica de JS';
+var c1 = a1+b1;
+
+console.log(c1);
+
+
+
+//a.3
+
+var a2 = 'Mi nombre es';
+var b2 = 'Martin Pueblas';
+var c2 = a2.length + b2.length;
+
+console.log(c2);

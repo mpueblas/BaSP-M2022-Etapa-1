ede5a4b802f15c4cb4dd7ecd5d28e552368e33c2
Martin Pueblas <war.pueblas@gmail.com>
2022-04-16 20:35:57 -0300

    BaSP-W05-2022: ex02-of_else

---
+++ Semana-05/excercises/04_if_else.js
@@ -0,0 +1,22 @@
+//4.
+var a = Math.random()*1;
+    if(a >= 0,5){
+        console.log('Greater than 0,5');
+    }else{
+        console.log('Lower than 0,5');
+    }
+//b.
+var b = 15;
+    if(b<2){
+        console.log('Bebe');
+    }else if((b>=2) && (b<=12)){
+        console.log('Niño');
+    }else if ((b>13) && (b<=19)){
+        console.log('Adolescente');
+    }else if ((b>=20) && (b<=30)){
+        console.log('Joven');
+    }else if ((b>=31) && (b<=60)){
+        console.log('Adulto');
+    }else if ((b>=61) && (b<=75)){
+        console.log('Adulto mayor');
+    }else ('Anciano');

f716d705e2e6dd823a862f16b1f09a513af7265a
Martin Pueblas <war.pueblas@gmail.com>
2022-04-16 20:28:24 -0300

    BaSP-W05-2022: ex03- arrays

---
+++ Semana-05/excercises/03_arrays.js
@@ -0,0 +1,11 @@
+//3.Arrays
+
+//a.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+console.log(meses [5] +' '+ meses [11]);
+
+//b.
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+ 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+console.log(meses.sort());

70be9c4e6b4039906878aa8af26282bff1ad2dda
Martin Pueblas <war.pueblas@gmail.com>
2022-04-26 18:33:49 -0300

    BaSP-W06-2022: I finished the Log-in validation

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -3,6 +3,10 @@ window.onload = function(){
     var form = document.getElementById('form-login');
     var inputs = document.getElementsByClassName('form-inpt');
     var regexMail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    var boxGood = document.getElementById('form-good');
+    var mailValue = document.getElementById('email');
+    var passValue = document.getElementById('password');
+    var wrongValidation = document.getElementById('form-mssg');
     
     
 
@@ -11,28 +15,24 @@ window.onload = function(){
             document.getElementById('div-email').classList.remove('form-grp-wrng');
             document.getElementById('div-email').classList.add('form-grp-ok');
             document.querySelector('#div-email .error-text').classList.remove('error-text-active');
+            return true
         } else{
             document.getElementById('div-email').classList.add('form-grp-wrng');
             document.getElementById('div-email').classList.remove('form-grp-ok');
             document.querySelector('#div-email .error-text').classList.add('error-text-active');
+            return false
         }
     };
     
     function resetForm(e){
-       /* document.getElementById('div-email').classList.remove('form-grp-wrng');
-        document.querySelector('#div-email .error-text').classList.remove('error-text-active');*/
+        document.getElementById('div-email').classList.remove('form-grp-wrng');
+        document.querySelector('#div-email .error-text').classList.remove('error-text-active');
     }
 
     inputs[0].addEventListener('blur', formValidation);
     inputs[0].addEventListener('focus', resetForm);    
 
-    form.addEventListener('submit', function(e){
-        e.preventDefault();
-    });
 
-
-    console.log(inputs);
-//
     function passValidation(){
         var numbers = ['0','1','2','3','4','5','6','7','8','9'];
         var num = 0;
@@ -44,17 +44,13 @@ window.onload = function(){
                 char++;
             }
         }
-        console.log(inputs[1].value.length >= 8);
-        console.log(num >= 1);
-        console.log(char >= 1);
         if (inputs[1].value.length >= 8 && num >= 1 && char >= 1){
             return true;
         }else {
             return false;
         }
     }
-    console.log(passValidation(), "hola")
-    console.log(inputs[1].value);
+
     function passwordValidation() {
         if(passValidation() == true){
             document.getElementById('div-password').classList.remove('form-grp-wrng');
@@ -68,4 +64,28 @@ window.onload = function(){
     };
     inputs[1].addEventListener('blur', passwordValidation);
     inputs[1].addEventListener('focus', resetForm);
+
+    var button = document.getElementsByClassName('button-2');
+    
+    form.onsubmit = function (e){
+        e.preventDefault();
+        if(formValidation() == true && passValidation() == true){
+            boxGood.classList.remove('hide');
+            boxGood.innerHTML = 'mail: ' + mailValue.value + '/ password: ' + passValue.value;
+        }else {
+            wrongValidation.classList.remove('hide');
+        }
+    }
+    console.log(mailValue.value, passValue.value);
+    
+    
+    
+    /*    
+    function validationBotton(){
+        if(formValidation() == true && passValidation() ==true){    
+            console.log(daleee)
+            return true;
+        }
+    }
+    button.addEventListener('submit', validationBotton);*/
 }
